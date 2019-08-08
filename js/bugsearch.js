fcf7ba0598ab40731dffa09eff802eb98d5d5f13
Alipoodle M <ali.poodle12@gmail.com>
2019-08-08 16:31:39 +0100

    Added searching (Need's layout)

--- js/bugsearch.js
+++ js/bugsearch.js
@@ -1,11 +1,81 @@
-var trelloKey;
-var trelloToken;
-if (typeof(Storage) !== 'undefined') {
-    trelloKey = localStorage.getItem('key');
-    trelloToken = localStorage.getItem('token');
+(function () {
+    var trelloKey;
+    var trelloToken;
+    if (typeof(Storage) !== 'undefined') {
+        trelloKey = localStorage.getItem('trello-key');
+        trelloToken = localStorage.getItem('trello-token');
+        $('#input-key').val(trelloKey);
+        $('#input-token').val(trelloToken);
+    }
+    else {
+        alert('This browser does not support Localstorage. Keys/Tokens will have to be manually inputted in each time.');
+    }
+
+    // Show Trello Token
+    if (!trelloKey || trelloKey == "" || !trelloToken || trelloToken == "") {
+        $(document).ready(function () {
+            $('#btn-trello-modal').trigger('click');
+        });
+    }
+})();
+
+function keySearch(evt) {
+    var $target = $(evt.target);
+    var keyCode = evt.which || evt.keyCode;
+    if (keyCode == 13) {
+        $target.blur(); // Auto triggers - searchTrello();
+    }
 }
-else {
-    alert('This browser does not support Localstorage. Keys/Tokens will have to be manually inputted in each time.');
+
+function searchTrello() {
+    var query = $('#search-field').val();
+    if (query.trim() == "") { return alert('Please fill some info in.'); }
+
+    var board = $('#board-field').val();
+    var trelloKey = $('#input-key').val();
+    var trelloToken = $('#input-token').val();
+
+    var options = {
+        method: 'GET',
+        url: 'https://api.trello.com/1/search',
+        data: {
+          query: query,
+          idBoards: board,
+          key: trelloKey,
+          token: trelloToken,
+
+          boards_limit: '15',
+          card_list: true,
+          card_attachments: true
+          /* Fill this */
+
+        }
+      };
+    $.ajax(options)
+      .done(function (data) {
+        $('#report-list').empty();
+        if (data.cards.length == 0) { return alert('No Cards were returned.'); }
+        data.cards.forEach(function (card) {
+            $('#report-list').append(
+                '<div class="report-item callout mbox">' +
+                    '<a class="report-title" href="' + card.shortUrl + '">' + card.name + '</a>' +
+                    '<p class="report-content">' +
+                        (card.closed ? '<strong>This ticket is archived.</strong><br>' : '') +
+                        (card.list.name ? '<strong>List:</strong> ' + card.list.name + '' : '') +
+                    '</p>' +
+                '</div>'
+            );
+        })
+
+      })
+      .fail(function () {
+        // Error
+      })
+}
+
+function updateTrello() {
+    localStorage.setItem('trello-key',   $('#input-key').val());
+    localStorage.setItem('trello-token', $('#input-token').val());
 }
 
 // ==============================================
@@ -53,29 +123,30 @@ function pageLoad(page) {
     var cb_btn = '';
     var st = '';
     switch (page) {
-        case "b":
-            cb_btn = '#copy-btn';
-            st = '#syntax';
-            break;
-        case "a":
-            cb_btn = '#edit-copy-btn';
-            st = '#edit-syntax';
+        case "index":
+            $('body').on('blur', 'input[id*="input-"]', updateTrello);
+            $('body').on('blur', '#search-field', searchTrello);
+            $('body').on('keypress', '#search-field', keySearch)
             break;
     }
-    var cb = new ClipboardJS(cb_btn, {
-        text: function(trigger) {
-            return $(st).text();
-        }
-    });
-    cb.on('success', function(e) {
-        $(e.trigger).html('Copied');
-        ga('send', 'event', 'syntax', 'copy');
-        setTimeout(function() {
-            $(e.trigger).html('Copy');
-        }, 2000);
-    });
     $('body').on('click', 'a[id*="switch-"]', switchMode);
     if (loadTheme()) {
         switchMode();
     }
 }
+
+// cb_btn = '#edit-copy-btn';
+// st = '#edit-syntax';
+
+// var cb = new ClipboardJS(cb_btn, {
+//     text: function(trigger) {
+//         return $(st).text();
+//     }
+// });
+// cb.on('success', function(e) {
+//     $(e.trigger).html('Copied');
+//     ga('send', 'event', 'syntax', 'copy');
+//     setTimeout(function() {
+//         $(e.trigger).html('Copy');
+//     }, 2000);
+// });
